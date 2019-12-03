017881e22bb1cc540a5a58a97e4c68065c53ecd4
nicolasraffray <nraffray@gmail.com>
2019-12-03 16:51:58 +0000

    this is the directory with questions up to and including q2

---
+++ directory2.rb
@@ -0,0 +1,52 @@
+# first we print the list of students
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Micheal Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
+end
+
+def print(names)
+  names.each_with_index do |student, index|
+    if student[:name][0] == 'A'
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+students = input_students
+print(students)
+print_footer(students)
