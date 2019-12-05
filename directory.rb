3089a1d6dbb7af199a741aa537d8726ca288504c
nicolasraffray <nraffray@gmail.com>
2019-12-05 18:25:35 +0000

    Q1&2: setting imput default and refactoring pushing elements into student array

--- directory.rb
+++ directory.rb
@@ -15,41 +15,19 @@
 
 @students = []
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return"
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = STDIN.gets.chomp
-  end
-end
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
-end
-
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
-
 # nothing happens until we call the methods
 # students = input_students
 # print(students)
 # print_footer(students)
 
 # ---- New Exercise ----
+def interactive_menu
+  load_students
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
 
 def print_menu
   puts "1. Input the students"
@@ -58,45 +36,6 @@ def print_menu
   puts "4. Load the list form students.csv"
   puts "5. Load Data as arguement from command line"
   puts "9. Exit" # 9. bc there will likely be more elements added
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of Students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def try_load_students
-  filename = ARGV.first # first arguement from the command line
-  return if filename.nil? # exit the method if the file name doesn't exist
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't esixt."
-    exit
-  end
 end
 
 def process(selection)
@@ -118,12 +57,76 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
+def fill_students_array
+  @students << {name: @name, cohort: :november}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return"
+  # get the first name
+  @name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    fill_students_array()
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    @name = STDIN.gets.chomp
   end
 end
 
-try_load_students()
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of Students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    @name, cohort = line.chomp.split(',')
+    fill_students_array()
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first arguement from the command line
+  return if filename.nil? # exit the method if the file name doesn't exist
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't esixt."
+    exit
+  end
+end
+
 interactive_menu()
